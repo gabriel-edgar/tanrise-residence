ecd08fae6d21e87169559ad264f8f4219ab4b809
Edgar <gabriel.nugroho@ifca.co.id>
2026-09-09 10:29:36 +0700

    Add Update for Android 13 or Lower

--- android/app/src/main/java/com/rn79/MainApplication.kt
+++ android/app/src/main/java/com/rn79/MainApplication.kt
@@ -12,32 +12,85 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
+// ---> Imports for SSL Bypass <---
+import com.facebook.react.modules.network.OkHttpClientFactory
+import com.facebook.react.modules.network.OkHttpClientProvider
+import okhttp3.OkHttpClient
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-            }
+    object : DefaultReactNativeHost(this) {
+      override fun getPackages(): List<ReactPackage> =
+        PackageList(this).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // add(MyReactNativePackage())
+        }
 
-        override fun getJSMainModuleName(): String = "index"
+      override fun getJSMainModuleName(): String = "index"
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
+      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    }
 
   override val reactHost: ReactHost
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
+    try {
+      val trustAllCerts = arrayOf<TrustManager>(
+        object : X509TrustManager {
+          override fun checkClientTrusted(
+            chain: Array<X509Certificate>,
+            authType: String
+          ) {}
+
+          override fun checkServerTrusted(
+            chain: Array<X509Certificate>,
+            authType: String
+          ) {}
+
+          override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+        }
+      )
+
+      val sslContext = SSLContext.getInstance("TLS")
+      sslContext.init(
+        null,
+        trustAllCerts,
+        java.security.SecureRandom()
+      )
+
+      OkHttpClientProvider.setOkHttpClientFactory(
+        object : OkHttpClientFactory {
+          override fun createNewNetworkModuleClient(): OkHttpClient {
+            return OkHttpClientProvider
+              .createClientBuilder()
+              .sslSocketFactory(
+                sslContext.socketFactory,
+                trustAllCerts[0] as X509TrustManager
+              )
+              .hostnameVerifier { _, _ ->
+                true
+              }
+              .build()
+          }
+        }
+      )
+    } catch (e: Exception) {
+      e.printStackTrace()
+    }
+
     SoLoader.init(this, OpenSourceMergedSoMapping)
+
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
   }
